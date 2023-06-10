6c6b86a1f7afb273c55e3bf86711bc18198e64fe
Yetrina Battad <hello@yetti.io>
2023-06-10 13:36:28 +1000

    chore: initial commit

---
+++ .history/config/application_20230610134506.rb
@@ -0,0 +1,29 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+# Load dotenv only in development or test environment
+if %w[development test].include? ENV["RAILS_ENV"]
+  Dotenv::Railtie.load
+end
+
+module ApeboardNeo
+  VERSION = "0.0.0"
+
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    config.time_zone = "Canberra"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
